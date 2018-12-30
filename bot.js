3979838300e8098ec4785c1de39ab36513cc26dd
zoma4 <45948124+zoma4@users.noreply.github.com>
2018-12-30 22:27:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 
 
-client.login("your token");
+client.login("NTEyNjI1OTgyNzUxMTEzMjE2.Dv2pAA.M-goF6BO9W_MMrDMtBLSMoAZs8A");
 
 
 
@@ -24,36 +24,36 @@ client.user.setStatus("dnd");
 
 
 
-const devs = ["your id"];
-const adminprefix = ["prefix"];
+const devs = ["512625982751113216"];
+const adminprefix = ["-"];
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
-  if (message.content.startsWith('(prefix))ply')) {
+  if (message.content.startsWith('-ply')) {
     client.user.setGame(argresult);
       message.channel.send(`**?   ${argresult}**`)
   } else 
      if (message.content === ("leave")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith('(prefix)wt')) {
+  if (message.content.startsWith('-wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**?   ${argresult}**`)
   } else 
-  if (message.content.startsWith('mils')) {
+  if (message.content.startsWith('-ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**?   ${argresult}**`)
   } else 
-  if (message.content.startsWith('(prefix)st')) {
+  if (message.content.startsWith('-st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/idk");
       message.channel.send(`**?**`)
   }
-  if (message.content.startsWith('(prefix)setname')) {
+  if (message.content.startsWith('-setname')) {
   client.user.setUsername(argresult).then
       message.channel.send(`Changing The Name To ..**${argresult}** `)
 } else
-if (message.content.startsWith('(prefix)setavatar')) {
+if (message.content.startsWith('-setavatar')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
